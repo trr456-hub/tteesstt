678559fe75ef6f91f98d24cba188e06f5c95bb01
gorhf9397 <trr456@naver.com>
2024-01-16 20:03:02 +0900

    test

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, { text: action.text, id: Date.now() }];
+    case DELETE_TODO:
+      return;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+const paintToDos = () => {
+  const todos = store.getState();
+  ul.innerHTML = "";
+  todos.forEach((item) => {
+    const li = document.createElement("li");
+    li.id = item.id;
+    li.innerText = item.text;
+    ul.appendChild(li);
+  });
+};
+
+store.subscribe(paintToDos);
+
+// const createTodo = (toDo) => {
+//   const li = document.createElement("li");
+//   li.innerText = toDo;
+//   ul.appendChild(li);
+// };
+
+const addTodo = (text) => {
+  store.dispatch({ type: ADD_TODO, text });
+};
+
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  addTodo(toDo);
+};
+
+form.addEventListener("submit", onSubmit);
