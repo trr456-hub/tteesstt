7c1cdfe08d36280b94b95fc0834a895ab9e733cb
seokgeun <trr456@naver.com>
2024-01-17 12:05:24 +0900

    react redux todo 구현

--- src/index.js
+++ src/index.js
@@ -7,12 +7,26 @@ const ul = document.querySelector("ul");
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+const addTodo = (text) => {
+  return {
+    type: ADD_TODO,
+    text,
+  };
+};
+const deleteTodo = (id) => {
+  // console.log(id);
+  return {
+    type: DELETE_TODO,
+    id,
+  };
+};
+
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [...state, { text: action.text, id: Date.now() }];
+      return [{ text: action.text, id: Date.now() }, ...state];
     case DELETE_TODO:
-      return;
+      return state.filter((toDo) => toDo.id !== action.id);
     default:
       return state;
   }
@@ -20,13 +34,26 @@ const reducer = (state = [], action) => {
 
 const store = createStore(reducer);
 
+const dispatchAddTodo = (text) => {
+  store.dispatch(addTodo(text));
+};
+const dispatchDeleteTodo = (e) => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteTodo(id));
+};
+
 const paintToDos = () => {
   const todos = store.getState();
   ul.innerHTML = "";
+  // console.log((ul.innerHTML = ""));
   todos.forEach((item) => {
     const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", dispatchDeleteTodo);
     li.id = item.id;
     li.innerText = item.text;
+    li.appendChild(btn);
     ul.appendChild(li);
   });
 };
@@ -39,15 +66,11 @@ store.subscribe(paintToDos);
 //   ul.appendChild(li);
 // };
 
-const addTodo = (text) => {
-  store.dispatch({ type: ADD_TODO, text });
-};
-
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  addTodo(toDo);
+  dispatchAddTodo(toDo);
 };
 
 form.addEventListener("submit", onSubmit);
